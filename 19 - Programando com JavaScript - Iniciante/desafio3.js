7d348e2ed6f3d6a4376f0976a814fcfff77650f9
DarthProg <dailoinf@gmail.com>
2022-01-21 20:09:59 -0300

    Jogo de Naves

---
+++ 19 - Programando com JavaScript - Iniciante/desafio3.js
@@ -0,0 +1,9 @@
+let numero = 10;
+for (i = 0; i <= numero; i++) {
+    let teste = i;
+    if (teste % 2 !==0){
+        console.log(teste);
+    } else {
+        console.log(`O número ${teste} não é ímpar!`);
+    }
+};
---
+++ 19 - Programando com JavaScript - Iniciante/testedesafio3.js
@@ -0,0 +1,8 @@
+let array = [1,2,3,4,5,6,7];
+for ( i=0; i<array.length; i++) {
+    if (array[i] % 2 !== 0) { 
+        console.log(array[i]) 
+    } else { 
+        console.log("não é impar") 
+    }
+};
